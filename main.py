0702d4d227bc38572ed4439f70814f1d45fa0d80
SzymonGozdowski <gozdosz@gmail.com>
2025-04-17 00:00:17 +0200

    Back bone of simulation, although with logical flaws

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import numpy as np
 
-half_length = 5
-river = np.array([x**2 for x in range(-half_length, half_length)])
 
+def water_flow(canal, fluid):
+    # function to set next volumes in river
+    # if there is a flood return False
+    source = 200
+    fluid_next = np.zeros(len(fluid))
+    fluid_next[0] = source
+    fluid_next[1:] = fluid[:len(fluid)-1]
+    for i in range(len(canal)):
+        if canal[i] < fluid[i]:
+            print("There is a flood")
+            return False
+    return True
+
+
+length = 10
+# length of river array represents length of the river
+# values inside represent dV possible to hold
+# if dV of water is bigger than dV of river
+# there will be a flood
+river = np.array([1 for x in range(0, length)])
+water = np.zeros(length)
+time = 20
+
+for t in range(time):
+    if not water_flow(river, water):
+        break
+else:
+    print("There was no flood")
