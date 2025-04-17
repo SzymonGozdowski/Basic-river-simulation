b7fdeeb8318eff519e7097aa1822f9bb029ee61f
SzymonGozdowski <gozdosz@gmail.com>
2025-04-17 21:36:32 +0200

    main

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import numpy as np
 def water_flow(canal, fluid):
     # function to set next volumes in river
     # if there is a flood return False
-    source = 200
+    source = 2
     fluid_next = np.zeros(len(fluid))
     fluid_next[0] = source
     fluid_next[1:] = fluid[:len(fluid)-1]
     for i in range(len(canal)):
         if canal[i] < fluid[i]:
             print("There is a flood")
-            return False
-    return True
+            return False, canal, fluid_next
+    return True, canal, fluid_next
 
 
 length = 10
@@ -20,12 +20,15 @@ length = 10
 # values inside represent dV possible to hold
 # if dV of water is bigger than dV of river
 # there will be a flood
-river = np.array([1 for x in range(0, length)])
+river = np.array([5 for x in range(0, length)])
 water = np.zeros(length)
 time = 20
 
 for t in range(time):
-    if not water_flow(river, water):
+    # for wat in water:
+    #    print(wat)
+    flood, river, water = water_flow(river, water)
+    if not flood:
         break
 else:
     print("There was no flood")
